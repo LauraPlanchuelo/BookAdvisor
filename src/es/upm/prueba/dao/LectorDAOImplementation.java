c0036b4dd210a43ec76ffce42f7bafced689bec0
BookAdvisor <29163151+BookAdvisor@users.noreply.github.com>
2018-04-17 19:25:43 +0200

    Update LectorDAOImplementation.java

--- src/es/upm/prueba/dao/LectorDAOImplementation.java
+++ src/es/upm/prueba/dao/LectorDAOImplementation.java
@@ -1,1 +1,17 @@
 
+package es.upm.prueba.dao;
+
+import java.util.List;
+
+import es.upm.prueba.model.Lector;
+
+public interface LectorDAOImplementation implements LectorDAO {
+
+	public void create(Lector lector);
+	public Lector read(String email);
+	public Lector update(Lector lector);
+	public void delete(Lector lector);
+	public List<Lector> getAll();
+	public Lector login(String email, String password);
+
+}
