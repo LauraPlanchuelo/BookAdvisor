345b145593e7bc1a30e23e698bbffe80dbb0a5a3
BookAdvisor <29163151+BookAdvisor@users.noreply.github.com>
2018-04-17 20:07:48 +0200

    Update BibliotecaDAOimplementation.java

--- src/es/upm/prueba/dao/BibliotecaDAOimplementation.java
+++ src/es/upm/prueba/dao/BibliotecaDAOimplementation.java
@@ -24,10 +24,57 @@ public class BibliotecaDAOimplementation implements BibliotecaDAO {
 				session.close();
 		}
 		
-	public Biblioteca read(String titulo);
-	public Biblioteca update(Biblioteca biblioteca);
-	public void delete(Biblioteca biblioteca);
-	public List<Biblioteca> getAll();
+	public Biblioteca read(String titulo){
+		Session session = SessionFactoryService.get().openSession();
+		Biblioteca biblioteca = null;
+		try {
+			biblioteca = session.get(biblioteca.class, titulo);
+		} catch (Exception e) {
+		} finally {
+			session.close();
+		}		
+		return biblioteca;
+	}
+		
+	public Biblioteca update(Biblioteca biblioteca){
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			session.saveOrUpdate(biblioteca);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+		} finally {
+			session.close();
+		
+	}
+		
+	public void delete(Biblioteca biblioteca){
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			session.delete(biblioteca);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+		// TODO: handle exception
+		} finally {
+			session.close();
+		}
+		
+	}
+		
+	public List<Biblioteca> getAll(){
+		Session session = SessionFactoryService.get().openSession();
+		List<Biblioteca> bibliotecas = new ArrayList<>();
+		try {
+			session.beginTransaction();
+			bibliotecas.addAll(session.createQuery("select t from Biblioteca t”).getResultList() );
+			session.getTransaction().commit();
+		} catch (Exception e) {
+		} finally {
+			session.close();
+		}
+		return bibliotecas;
+	}
 	
 	public Biblioteca login(String email, String password){
 		Session session = SessionFactoryService.get().openSession();
