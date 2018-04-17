e580b95b1aae63bc2c4fc8a3bf2872f00a49cf27
BookAdvisor <29163151+BookAdvisor@users.noreply.github.com>
2018-04-17 19:27:35 +0200

    Update LectorDAOImplementation.java

--- src/es/upm/prueba/dao/LectorDAOImplementation.java
+++ src/es/upm/prueba/dao/LectorDAOImplementation.java
@@ -6,6 +6,13 @@ import java.util.List;
 import es.upm.prueba.model.Lector;
 
 public interface LectorDAOImplementation implements LectorDAO {
+	public static BibliotecaDAOimplementation instance;
+	private BibliotecaDAOimplementation() { }
+	public static BibliotecaDAOimplementation getInstance() {
+		if ( null == instance )
+			instance = new BibliotecaDAOimplementacion();
+		return instance;
+	}
 
 	public void create(Lector lector);
 	public Lector read(String email);
