289a39ccdd64c3362a567fc02dc51d871ee83528
BookAdvisor <29163151+BookAdvisor@users.noreply.github.com>
2018-04-17 20:18:12 +0200

    Update NoticiaDAOImplementation.java

--- src/es/upm/prueba/dao/NoticiaDAOImplementation.java
+++ src/es/upm/prueba/dao/NoticiaDAOImplementation.java
@@ -5,19 +5,79 @@ import java.util.List;
 
 import es.upm.prueba.model.Noticia;
 
+@override
 public interface NoticiaDAOImplementation implements NoticiaDAO {
+	
 	public static NoticiaDAOimplementation instance;
-	private NoticiaDAOimplementation() { }
+	
+	private NoticiaDAOimplementation() { 
+	
+	}
+	
 	public static NoticiaDAOimplementation getInstance() {
 		if ( null == instance )
 			instance = new NoticiaDAOimplementacion();
 		return instance;
 	}
 
-	public void create(Noticia noticia);
-	public Noticia read(String titulo);
-	public Noticia update(Noticia noticia);
-	public void delete(Noticia noticia);
-	public List<Noticia> getAll();
+		public FichaLibro read(String titulo);
+	public FichaLibro update(FichaLibro ficha);
+	public void delete(FichaLibro ficha);
+	public List<FichaLibro> getAll();
+	
+	@override
+	public Noticia read(String titulo){
+		Session session = SessionFactoryService.get().openSession();
+		Noticia noticia = null;
+		try {
+			libro = session.get(noticia.class, titulo);
+		} catch (Exception e) {
+		} finally {
+				session.close();
+		}		
+		return libro;
+	}
+	
+	@override
+	public Noticia update(Noticia noticia){
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			session.saveOrUpdate(noticia);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+		} finally {
+			session.close();
+		}
+	}
+	
+	@override
+	public void delete(Noticia noticia){
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			session.delete(noticia);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+		// TODO: handle exception
+		} finally {
+		session.close();
+	}
+}
+	@override
+	public List<Noticia> getAll(){
+		Session session = SessionFactoryService.get().openSession();
+		List<Noticia> noticia = new ArrayList<>();
+		try {
+			session.beginTransaction();
+			fichas.addAll(session.createQuery("select t from Noticia t”).getResultList() );
+			session.getTransaction().commit();
+		} catch (Exception e) {
+		} finally {
+			session.close();
+		}
+		return fichas;
+	}
+	
 
 }
