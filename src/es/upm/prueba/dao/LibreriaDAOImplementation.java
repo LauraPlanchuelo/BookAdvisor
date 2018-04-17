8b1ae6a86f37589b1f992cb8d1f33a27795348d9
BookAdvisor <29163151+BookAdvisor@users.noreply.github.com>
2018-04-17 19:34:32 +0200

    Update LibreriaDAOImplementation.java

--- src/es/upm/prueba/dao/LibreriaDAOImplementation.java
+++ src/es/upm/prueba/dao/LibreriaDAOImplementation.java
@@ -19,6 +19,23 @@ public interface LibreriaDAOImplementation implements LibreriaDAO {
 	public Libreria update(Libreria libreria);
 	public void delete(Libreria libreria);
 	public List<Libreria> getAll();
-	public Libreria login(String email, String password);
+	public Libreria login(String email, String password){
+		Session session = SessionFactoryService.get().openSession();
+		Libreria libreria = null;
+		try {
+			session.beginTransaction();
+			tfg = (TFG) session.createQuery (
+			“select t from TFG t where t.email= :email and t.password= :password”).
+				
+			.setParameter(“email”, email).
+			.setParameter(“password”, password).uniqueResult();
+				session.getTransaction().commit();
+		} catch (Exception e) {
+		} finally {
+			session.close();
+		}
+		return libreria;
+	}
+	
 
 }
