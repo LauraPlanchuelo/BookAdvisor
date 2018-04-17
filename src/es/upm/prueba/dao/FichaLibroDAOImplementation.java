8bd73b06d14687a1566d8e9068bad7ece980c37f
BookAdvisor <29163151+BookAdvisor@users.noreply.github.com>
2018-04-17 19:27:06 +0200

    Update FichaLibroDAOImplementation.java

--- src/es/upm/prueba/dao/FichaLibroDAOImplementation.java
+++ src/es/upm/prueba/dao/FichaLibroDAOImplementation.java
@@ -6,6 +6,13 @@ import java.util.List;
 import es.upm.prueba.model.FichaLibro;
 
 public interface FichaLibroDAOImplementation implements FichaLibroDAO{
+	public static BibliotecaDAOimplementation instance;
+	private BibliotecaDAOimplementation() { }
+	public static BibliotecaDAOimplementation getInstance() {
+		if ( null == instance )
+			instance = new BibliotecaDAOimplementacion();
+		return instance;
+	}
 
 	public void create(FichaLibro ficha);
 	public FichaLibro read(String titulo);
