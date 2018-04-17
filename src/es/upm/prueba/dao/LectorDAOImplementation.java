f4682d266876fd92a162adcc8fb67cebee0c7162
BookAdvisor <29163151+BookAdvisor@users.noreply.github.com>
2018-04-17 20:06:41 +0200

    Update LectorDAOImplementation.java

--- src/es/upm/prueba/dao/LectorDAOImplementation.java
+++ src/es/upm/prueba/dao/LectorDAOImplementation.java
@@ -14,22 +14,63 @@ public interface LectorDAOImplementation implements LectorDAO {
 		return instance;
 	}
 
-	public void create(Lector lector);
-	public Lector read(String email);
-	public Lector update(Lector lector);
-	public void delete(Lector lector);
-	public List<Lector> getAll();
+	public Lector read(String titulo){
+		Session session = SessionFactoryService.get().openSession();
+		Lector lector = null;
+		try {
+			lector = session.get(lector.class, titulo);
+		} catch (Exception e) {
+		} finally {
+			session.close();
+		}		
+		return libro;
+	}		
+	public void update(Lector lector){
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			session.saveOrUpdate(ficha);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+		} finally {
+			session.close();
+		}
+	}
+	public void delete(Lector lector){
+		Session session = SessionFactoryService.get().openSession();
+		try {
+			session.beginTransaction();
+			session.delete(lector);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+		// TODO: handle exception
+		} finally {
+		session.close();
+		}
+	}
+	public List<Lector> getAll(){
+		Session session = SessionFactoryService.get().openSession();
+		List<Lector> fichas = new ArrayList<>();
+		try {
+			session.beginTransaction();
+			fichas.addAll(session.createQuery("select t from FichaLibro t”).getResultList());
+			session.getTransaction().commit();
+		} catch (Exception e) {
+		} finally {
+			session.close();
+		}
+		return fichas;
+	}
+
 	public Lector login(String email, String password){
 		Session session = SessionFactoryService.get().openSession();
 		Lector lector = null;
 		try {
 			session.beginTransaction();
-			lector = (Lector) session.createQuery (
-			“select t from Lector t where t.email= :email and t.password= :password”).
-				
-			.setParameter(“email”, email).
-			.setParameter(“password”, password).uniqueResult();
-				session.getTransaction().commit();
+			lector = (Lector) session.createQuery (“select t from Lector t where t.email= :email and t.password= :password”).
+				.setParameter(“email”, email).
+				.setParameter(“password”, password).uniqueResult();
+			session.getTransaction().commit();
 		} catch (Exception e) {
 		} finally {
 			session.close();
