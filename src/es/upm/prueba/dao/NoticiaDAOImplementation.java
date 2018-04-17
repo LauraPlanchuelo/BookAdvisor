6e23ca1bf3d431608719d23a6616ece96059fe5d
BookAdvisor <29163151+BookAdvisor@users.noreply.github.com>
2018-04-17 19:28:19 +0200

    Update NoticiaDAOImplementation.java

--- src/es/upm/prueba/dao/NoticiaDAOImplementation.java
+++ src/es/upm/prueba/dao/NoticiaDAOImplementation.java
@@ -6,6 +6,13 @@ import java.util.List;
 import es.upm.prueba.model.Noticia;
 
 public interface NoticiaDAOImplementation implements NoticiaDAO {
+	public static BibliotecaDAOimplementation instance;
+	private BibliotecaDAOimplementation() { }
+	public static BibliotecaDAOimplementation getInstance() {
+		if ( null == instance )
+			instance = new BibliotecaDAOimplementacion();
+		return instance;
+	}
 
 	public void create(Noticia noticia);
 	public Noticia read(String titulo);
