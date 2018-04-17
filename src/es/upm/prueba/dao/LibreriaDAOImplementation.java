e6286e52579720888ba124d125e4409a69d4200b
BookAdvisor <29163151+BookAdvisor@users.noreply.github.com>
2018-04-17 19:24:19 +0200

    Update LibreriaDAOImplementation.java

--- src/es/upm/prueba/dao/LibreriaDAOImplementation.java
+++ src/es/upm/prueba/dao/LibreriaDAOImplementation.java
@@ -1,1 +1,17 @@
 
+package es.upm.prueba.dao;
+
+import java.util.List;
+
+import es.upm.prueba.model.Libreria;
+
+public interface LibreriaDAOImplementation implements LibreriaDAO {
+
+	public void create(Libreria libreria);
+	public Libreria read(String email);
+	public Libreria update(Libreria libreria);
+	public void delete(Libreria libreria);
+	public List<Libreria> getAll();
+	public Libreria login(String email, String password);
+
+}
