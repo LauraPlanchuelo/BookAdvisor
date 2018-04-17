60fa040dff4a31c187acf94a692cae81c0c3f794
BookAdvisor <29163151+BookAdvisor@users.noreply.github.com>
2018-04-17 19:26:44 +0200

    Update BibliotecaDAOimplementation.java

--- src/es/upm/prueba/dao/BibliotecaDAOimplementation.java
+++ src/es/upm/prueba/dao/BibliotecaDAOimplementation.java
@@ -5,6 +5,13 @@ import java.util.List;
 import es.upm.prueba.model.Biblioteca;
 
 public class BibliotecaDAOimplementation implements BibliotecaDAO {
+	public static BibliotecaDAOimplementation instance;
+	private BibliotecaDAOimplementation() { }
+	public static BibliotecaDAOimplementation getInstance() {
+		if ( null == instance )
+			instance = new BibliotecaDAOimplementacion();
+		return instance;
+	}
 	
 	public void create(Biblioteca biblioteca);
 	public Biblioteca read(String titulo);
