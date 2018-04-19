622fcbad4fe37fe169e09d36e79145a29947fc05
BookAdvisor <l.maria-tome@alumnos.upm.es>
2018-04-19 13:39:16 +0200

    Added servlet for noticia creation

--- src/com/bookadvisor/servlets/CreateLectorServlet.java
+++ src/com/bookadvisor/servlets/CreateLectorServlet.java
@@ -17,7 +17,6 @@ public class CreateLectorServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String name = req.getParameter("name");
-		String surname = req.getParameter("surname");
 		String email = req.getParameter("email");
 		String password = req.getParameter("password");
 		
---
+++ src/com/bookadvisor/servlets/CreateNoticia.java
@@ -0,0 +1,30 @@
+package com.bookadvisor.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bookadvisor.dao.NoticiaDAOImplementation;
+import com.bookadvisor.dao.model.Noticia;
+
+@WebServlet("/CreateNoticia ")
+public class CreateNoticia extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String titulo = req.getParameter("titulo");
+		String autor = req.getParameter("pub_name");
+		String cuerpo = req.getParameter("noticia");
+		
+		Noticia noticia = new Noticia()
+								.setAutor(autor)
+								.setTitulo(titulo)
+								.setCuerpo(cuerpo);
+		
+		NoticiaDAOImplementation.getInstance().create(noticia);
+	}
+}
