1244d04e7f2b7c4e79b813a1ecd094092d953813
BookAdvisor <l.maria-tome@alumnos.upm.es>
2018-04-19 11:58:47 +0200

    Fixed LoginServlet location and imports

--- src/com/bookadvisor/servlets/LoginServlet.java
+++ src/com/bookadvisor/servlets/LoginServlet.java
@@ -1,4 +1,4 @@
-package com.bookadvisor.servlet;
+package com.bookadvisor.servlets;
 
 import java.io.IOException;
 
@@ -8,13 +8,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.bookadvisor.dao.ProfessorDAOImplementation;
-import com.bookadvisor.dao.TFGDAOImplementation;
-import com.bookadvisor.dao.model.Professor;
-import com.bookadvisor.dao.model.TFG;
+import com.bookadvisor.dao.LectorDAOImplementation;
+import com.bookadvisor.dao.LibreriaDAOImplementation;
+import com.bookadvisor.dao.model.Lector;
+import com.bookadvisor.dao.model.Libreria;
 
 @WebServlet ("/LoginServlet")
-
 public class LoginServlet extends HttpServlet {
 	private final String ADMIN_EMAIL = "root";
 	private final String ADMIN_PASSWORD = "root";
@@ -23,12 +22,12 @@ public class LoginServlet extends HttpServlet {
 
 		String email = req.getParameter("email");
 		String password = req.getParameter("password");
-		LECTOR lector = LectorDAOImplementation.getInstance().loginLector(email, password);
-		LIBRERIA libreria = LibreriaDAOImplementation.getInstance().loginLibreria(email, password);
+		Lector lector = LectorDAOImplementation.getInstance().login(email, password);
+		Libreria libreria = LibreriaDAOImplementation.getInstance().login(email, password);
 
 		if (ADMIN_EMAIL.equals(email) && ADMIN_PASSWORD.equals(password) ) {
 			req.getSession().setAttribute("lector_list", LectorDAOImplementation.getInstance().getAll());
-			resp.sendRedirect(req.getContextPath() + "/LoginSecretaria.jsp");//modificar
+			resp.sendRedirect(req.getContextPath() + "/LoginSecretaria.jsp");
 		} else if (null != lector) {
 			req.getSession().setAttribute("lector", lector);
 			resp.sendRedirect(req.getContextPath() + "/LoginLector.jsp");
